53908d018b013596c5a5dc52919ee90a83b3c323
Andy Pan <andy0130tw@yahoo.com.tw>
2025-11-20 15:21:45 +0800

    Nudge dep version bound

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Agda is a dependently typed programming language / interactive theorem prover.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/staging-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.ghc-wasm.url = "git+https://gitlab.haskell.org/haskell-wasm/ghc-wasm-meta.git";
 
